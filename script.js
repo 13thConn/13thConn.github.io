18324069ead832cbe56f89e61ac4c187b8f4943c
13thConn <100418122+13thConn@users.noreply.github.com>
2022-04-17 17:26:49 +0800

    Puzzle①

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function sayHello(){
+  var response = prompt("What's your name?");
+  alert("Hello,"+response+"~ You need to identify pictures to get digital resultse");
+}
+
+function Gettips(){
+  var response = alert("Each fruit represents a number.");
+}
+
+function Showanswer(){
+  alert("The answer is 16");
+}
+
+function answer(){
+  var response = prompt("Wow! Tell me your answer.");
+  var x = 16;
+if (response == x) {
+  alert("Wow! 16 is the right answer.You are so clever!")
+} else {
+alert("Unfortunately, this is not the right answer. You may need to think again.")
+}
+}
